aa689a6ef50fefbaa69f31a7d2f4849fea57a528
Marco McNulty <mmc90@hotmail.co.uk>
2019-07-02 11:51:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentence1)
-  words= sentence1.split
+def reverse_each_word(sentence2)
+  words= sentence2.split
   reversed_words = ""
   words.each do |word|
     reversed_words += word.reverse
@@ -7,8 +7,8 @@ def reverse_each_word(sentence1)
   reversed_words
 end
 
-def reverse_each_word(senence2)
-  words= sentence2.split
+def reverse_each_word(senence1)
+  words= sentence1.split
   reversed_words = words.collect { |word| word.reverse }
   reversed_words
 end
